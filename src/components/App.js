d94970ce914a94a92744de203f9769f6959e02eb
Janak kr shrestha <janakkrshrestha@gmail.com>
2024-10-18 20:07:48 +0545

    Displaying Questions and options

--- src/components/App.js
+++ src/components/App.js
@@ -12,6 +12,7 @@ const initialState = {
 
   // loading, error, ready, active, finished
   status: "loading",
+  index: 0,
 };
 
 function reducer(state, action) {
@@ -41,7 +42,10 @@ function reducer(state, action) {
 }
 
 export default function App() {
-  const [{ questions, status }, dispatch] = useReducer(reducer, initialState);
+  const [{ questions, status, index }, dispatch] = useReducer(
+    reducer,
+    initialState
+  );
   const numQuestions = questions.length;
 
   useEffect(function () {
@@ -61,7 +65,7 @@ export default function App() {
         {status === "ready" && (
           <StartScreen numQuestions={numQuestions} dispatch={dispatch} />
         )}
-        {status === "active" && <Question />}
+        {status === "active" && <Question questions={questions[index]} />}
       </Main>
     </div>
   );
---
+++ src/components/Question.js
@@ -0,0 +1,14 @@
+import Options from "./Options";
+
+function Question({ questions }) {
+  //   console.log(questions);
+  return (
+    <div>
+      <h4>{questions.question}</h4>
+
+      <Options questions={questions} />
+    </div>
+  );
+}
+
+export default Question;
